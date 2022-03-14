c92b14030b9d1fd7bd0533b54d42f590042684c2
jaco718v <jaco718v@stud.kea.dk>
2022-03-14 12:47:07 +0100

    Added Room constructer + direction methods

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,4 +1,63 @@
 package com.company;
 
 public class Room {
+  private String name;
+  private String beskrivelse;
+  private Room north;
+  private Room east;
+  private Room south;
+  private Room west;
+
+  public Room(String name, String beskrivelse, Room north, Room east, Room south, Room  west){
+    this.name=name;
+    this.beskrivelse=beskrivelse;
+    this.north=north;
+    this.east=east;
+    this.south=south;
+    this.west=west;
+  }
+
+  public Room goNorth(Room north){
+    if (north.getNorth()==null){
+      return null;}
+      else
+        return north.getNorth();
+  }
+
+  public Room goEast(Room east){
+    if (east.getEast()==null){
+      return null;}
+    else
+      return east.getEast();
+  }
+
+  public Room goSouth(Room south){
+    if (south.getSouth()==null){
+      return null;}
+    else
+      return south.getSouth();
+  }
+
+  public Room goWest(Room west){
+    if (west.getWest()==null){
+      return null;}
+    else
+      return west.getWest();
+  }
+
+  public Room getWest() {
+    return west;
+  }
+
+  public Room getNorth() {
+    return north;
+  }
+
+  public Room getSouth() {
+    return south;
+  }
+
+  public Room getEast() {
+    return east;
+  }
 }
